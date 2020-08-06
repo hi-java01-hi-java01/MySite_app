69e7196adbe4216de06d670db858968434d10fd3
iremys <iremys@gmail.com>
2020-08-06 12:10:22 +0900

    1st

---
+++ app/src/main/java/com/javaex/mysite/GuestWriteForm.java
@@ -0,0 +1,17 @@
+package com.javaex.mysite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GuestWriteForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guest_write_form);
+    }
+
+
+    /*주석*/
+}
